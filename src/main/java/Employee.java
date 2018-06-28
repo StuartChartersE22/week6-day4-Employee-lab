cee52433062541867eacdd57e8be3c805fde1332
Magdalena <m.mandat@gmail.com>
2018-06-28 14:26:40 +0100

    set up employee as an abstract class

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,2 +1,32 @@
-public class Employee {
+public abstract class Employee {
+
+    private String name;
+    private int ni;
+    protected double salary;
+
+    public Employee(String name, int ni, double salary){
+        this.name = name;
+        this.ni = ni;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getNi() {
+        return this.ni;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    public void riaiseSalary(double amount){
+        this.salary += amount;
+    }
+
+    public double payBonus(){
+        return this.salary * 0.01;
+    }
 }
