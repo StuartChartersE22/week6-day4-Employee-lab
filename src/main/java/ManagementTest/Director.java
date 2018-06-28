243d6b14ef8ff5c387eacd1179069cb0dc211908
Magdalena <m.mandat@gmail.com>
2018-06-28 15:32:35 +0100

    director has all the methods of a manager

--- src/main/java/ManagementTest/Director.java
+++ src/main/java/ManagementTest/Director.java
@@ -1,4 +1,12 @@
 package ManagementTest;
 
-public class Director {
+public class Director extends Manager {
+
+    private double budget;
+    public Director(String name, int ni, double salary, String deptname, double budget){
+        super(name, ni, salary, deptname);
+        this.budget = budget;
+    }
+
+
 }
--- src/test/java/ManagementTest/DirectorTest.java
+++ src/test/java/ManagementTest/DirectorTest.java
@@ -1,4 +1,47 @@
 package ManagementTest;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DirectorTest {
+
+    private Director director;
+
+    @Before
+    public void before(){
+        director = new Director("Lisa", 5, 123.20, "Shop floor", 2000.30);
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Lisa", director.getName());
+    }
+
+    @Test
+    public void canGetNI(){
+        assertEquals(5, director.getNi());
+    }
+
+    @Test
+    public void canGetSalary(){
+        assertEquals(123.20, director.getSalary(), 0.001);
+    }
+
+    @Test
+    public void getDeptname(){
+        assertEquals("Shop floor", director.getDeptname());
+    }
+
+    @Test
+    public void canRaiseSalary(){
+        director.raiseSalary(5.50);
+        assertEquals(128.70, director.getSalary(), 0.001);
+    }
+
+    @Test
+    public void canGetPayBonus(){
+        assertEquals(1.232, director.payBonus(), 0.001);
+    }
 }
