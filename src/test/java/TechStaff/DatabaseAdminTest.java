58cf187e35c0055a1e779bed84e91788fd984626
Magdalena <m.mandat@gmail.com>
2018-06-28 15:23:41 +0100

    database admin tested

--- src/test/java/TechStaff/DatabaseAdminTest.java
+++ src/test/java/TechStaff/DatabaseAdminTest.java
@@ -1,6 +1,9 @@
 package TechStaff;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class DatabaseAdminTest {
 
@@ -11,4 +14,31 @@ public class DatabaseAdminTest {
         databaseAdmin = new DatabaseAdmin("Oscar", 3, 18.00);
     }
 
+    @Test
+    public void canGetName(){
+        assertEquals("Oscar", databaseAdmin.getName());
+    }
+
+    @Test
+    public void canGetNI(){
+        assertEquals(3, databaseAdmin.getNi());
+    }
+
+    @Test
+    public void canGetSalary(){
+        assertEquals(18.00, databaseAdmin.getSalary(), 0.001);
+    }
+
+    @Test
+    public void canRaiseSalary(){
+        databaseAdmin.raiseSalary(5.50);
+        assertEquals(23.50, databaseAdmin.getSalary(), 0.001);
+    }
+
+    @Test
+    public void canGetPayBonus(){
+        assertEquals(0.18, databaseAdmin.payBonus(), 0.001);
+    }
+
 }
+
